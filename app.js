676e94e96409dbfe3f1a6aae8b52dfc4f808f993
ben simms <info@bensimms.co.uk>
2020-04-23 13:01:01 +0100

    Testing reset

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+// Loads express and storage library
+const express = require('express');
+const storage = require('node-persist');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('1');
+// Holds the current views
+var counter = 0;
+
+// Create a new Express app instance
+var app = express();
+app.use(express.static('./static'));
+
+// Registers an event triggered on HTTP GET /visit
+app.get("/reset", (req, res) => {
+    counter++;
+    
+    // Saves counter into the store and send response AFTER the store has been saved
+    storage.setItem("counter", counter).then(() => {
+        res.json(counter);
+    });
 });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+// Inits permanent storage and reads the saved counter
+storage.init().then(() => storage.getItem("counter")).then((value) => {
+    // Checks if value read is valid, otherwise set it to 0
+    if (value > 0) {
+        counter = value;
+    } else {
+        counter = 0;
+    }
+
+    // Start the web server, listening on port 8080, AFTER the counter has been read
+    app.listen(3000, "0.0.0.0");
 });
